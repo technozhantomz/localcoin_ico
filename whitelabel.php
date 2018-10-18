6a48f8461ff9b1488c64993ef9c4af81d84d484e
Nikita Kunevich <procion95@gmail.com>
2018-10-18 13:57:24 +0300

    content update

---
+++ whitelabel.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <title>Localcoin - White Label</title>
+    <!-- CSS-->
+    <link href="css/style.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <div class="wrapper">
+      <header class="header header_inner" id="header">
+        <div class="container">
+          <div class="hamburger hamburger--squeeze">
+            <div class="hamburger-box">
+              <div class="hamburger-inner"></div>
+            </div>
+          </div><a class="header__logo" href="/"><img src="img/logo.png" srcset="img/logo-2x.png 2x" alt="Localcoin"></a>
+          <div class="header__nav" style="display:none">
+            <ul>
+              <li><a href="index.html#advantages">Why</a></li>
+              <li><a href="index.html#trading">Trading</a></li>
+              <li><a href="index.html#tokeninfo">Token info</a></li>
+              <li><a href="index.html#roadmap">Road map</a></li>
+              <li><a href="index.html#download">Download</a></li>
+            </ul>
+          </div>
+          <div class="header__right-menu">
+            <ul>
+              <li><a class="btn-blue btn-blue_small" id="buy_coin_header" href="#">Buy LLC</a></li>
+              <li><a class="btn-blue btn-blue_small sign-in" id="signIn" href="https://wallet.localcoin.is/">Sign In</a></li>
+              <li><a class="whitepaper" href="https://docs.localcoin.is/">WP</a></li>
+              <li><a class="whitelabel" href="/">WL</a></li>
+              <!-- <li> 
+                <div class="header__lang select select-lang">
+                  <input class="select_hidden" type="hidden" value="1">
+                  <div class="select_in select-lang_in"><span class="select_title select-lang_title"><img src="img/eng.png" srcset="img/eng-2x.png 2x" alt="eng"></span><i class="select_arrow select-lang_arrow"></i></div>
+                  <ul class="select_list select-lang_list">
+                    <li class="is-active" data-value="1"><img src="img/eng.png" srcset="img/eng-2x.png 2x" alt="eng"></li>
+                    <li data-value="2"><img src="img/svg/rus.svg" alt="rus"></li>
+                  </ul>
+                </div> -->
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      <section class="white-label" id="whitelabel">
+        <div class="container">
+          <ul class="breadcrumbs white-label__breadcrumbs">
+            <li><a>Home</a></li>
+            <li><a>White Label</a></li>
+          </ul>
+          <div class="row">
+            <div class="col-sm-6 col-md-8">
+              <h1 class="white-label__title"><span>White label </span> exchange solution for crypto businesses</h1><span class="white-label__subtitle">Launching your own cryptocurrency exchange? Tap our readymade decentralized platform with no hassle setup.</span>
+            </div>
+            <div class="col-sm-6 col-md-4">
+              <form class="white-label__form wl-form" action="#">
+                <h4 class="wl-form__title">Contact us to<br>learn more about<br>our white label offering</h4>
+                <div class="wl-form__input">
+                  <label for="input_name">Name*</label>
+                  <input id="input_name" type="text" name="name" required>
+                </div>
+                <div class="wl-form__input">
+                  <label for="input_email">Email*</label>
+                  <input id="input_email" type="text" name="email" required>
+                </div>
+                <div class="wl-form__input">
+                  <label for="message">Your message:</label>
+                  <textarea id="message" name="message"></textarea>
+                </div>
+                <div class="wl-form__input">
+                  <label class="checkbox">
+                    <input class="checkbox__default" type="checkbox" name="checkbox"><span class="checkbox__custom"></span><span class="checkbox__label">I agree with OpenLedger's <a>Privacy Policy</a></span>
+                  </label>
+                </div>
+                <input class="wl-form__submit" type="submit" value="Get in touch">
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="white-label__advantages wl-advantages">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6 col-md-4">
+                <div class="wl-advantages__item wl-advantages__item_01">
+                  <h5>High performance</h5>
+                  <p>A liquidity-ready trading platform to help you start a cryptocurrency exchange business in only 2 weeks. Over 50 coins, fiat gateways, unmatched security and localizations — all included, without investing heavily in development and infrastructure.</p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="wl-advantages__item wl-advantages__item_02">
+                  <h5>Top-notch technology</h5>
+                  <p>We made the best of what BitShares had to offer and built a new, even better-designed platform. Powered by Graphene, it can process more transactions every second than Bitcoin or Ethereum.</p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-6 col-md-4">
+                <div class="wl-advantages__item wl-advantages__item_03">
+                  <h5>Ready-made, customizable UI</h5>
+                  <p>Go with the original BitShares layouts or tap into our own, improved design. All tailored to your brand identity and regularly updated by our team. You can also provide deeper customization if needed.</p>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                <div class="wl-advantages__item wl-advantages__item_04">
+                  <h5>Scalability</h5>
+                  <p>Increase your revenues by implementing new crypto and fiat gateways as well as fees. You can do it in house or let OpenLedger do all the heavy lifting on a one-time payment or revenue-sharing basis.</p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-6 col-md-4">
+                <div class="wl-advantages__item wl-advantages__item_05">
+                  <h5>Professional product team</h5>
+                  <p>A team of 40+ blockchain and cryptocurrency experts is working every day to improve the platform. We are constantly rolling out new gateways to let users trade world’s most wanted coins. Rest assured we’ll have you covered from the moment your exchange goes live.</p>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                <div class="wl-advantages__item wl-advantages__item_06">
+                  <h5>Flexible engagement models</h5>
+                  <p>We’ve come up with 3 transparent engagement models, each with a different level of control and risk. You can start small and then switch to a more high-profit model as your business grows.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8">
+              <div class="white-label__fees">
+                <h3>Fees</h3>
+                <p>OpenLedger DEX relies on BitShares’ fee structure. It means that you will retain 80% of the fee a registered user pays for any transaction with the system. See this document to learn more about BitShares’ fees.</p>
+              </div>
+              <div class="white-label__tech-req tech-req">
+                <h3>Technical requirements</h3>
+                <p>Our platform requires no heavy infrastructure investments. All you need to get your exchange up and running is:</p>
+                <ul class="tech-req__list">
+                  <li class="tech-req__list-item tech-req__list-item_01">Registered domain name</li>
+                  <li class="tech-req__list-item tech-req__list-item_02">SSL certificate</li>
+                  <li class="tech-req__list-item tech-req__list-item_03">2 servers with the following minimum configuration:</li>
+                </ul>
+                <div class="tech-req__table">
+                  <table>
+                    <tr>
+                      <td class="t-title">Processor</td>
+                      <td>Intel® Xeon® E5-1650 v3 Hexa-Core</td>
+                    </tr>
+                    <tr>
+                      <td class="t-title">RAM</td>
+                      <td>256GB DDR4 ECC</td>
+                    </tr>
+                    <tr>
+                      <td class="t-title">Data Storage</td>
+                      <td>2×4TB SATA 6Gb/s Enterprise HDD; 7200 rpm</td>
+                    </tr>
+                    <tr>
+                      <td class="t-title">Ethernet</td>
+                      <td>1 Gbit/s bandwidth</td>
+                    </tr>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="getfree" id="getfree">
+        <div class="container">
+          <div class="getfree__logo"><img src="img/d-logo.png" srcset="img/d-logo-2x.png 2x" alt="logo"></div>
+          <h2 class="wow fadeInUp">Create your account for <span>free </span>and start trading now.</h2>
+          <p class="wow fadeInUp">The first 100 000 users receive 100 LLC coins after registration on the exchange's website</p><a class="getfree__btn btn-yellow" href="#">Join and start trading now</a>
+        </div>
+      </section>
+      <footer class="footer" id="footer">
+        <div class="container">
+          <ul class="footer__social f-social">
+            <li><a class="f-social__item f-social__item_vk" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_fb" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_tm" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_04" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_05" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_06" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_07" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_08" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_bt" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_tw" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_yt" href="#"></a></li>
+            <li><a class="f-social__item f-social__item_in" href="#"></a></li>
+          </ul>
+          <ul class="footer__links">
+            <li><a class="footer__wl" href="whitelabel.html">White Label</a></li>
+            <li><a class="footer__wp" href="https://docs.localcoin.is">White Paper</a></li>
+            <li><a class="btn-blue" id="buy_coin_footer" href="#">Buy LLC coin</a></li>
+          </ul>
+          <ul class="footer__nav wow fadeInUp">
+            <li><a href="index.html#main">Home</a></li>
+            <li><a href="index.html#advantages">Why</a></li>
+            <li><a href="index.html#trading">Trading</a></li>
+            <li><a href="index.html#tokeninfo">Token info</a></li>
+            <li><a href="index.html#roadmap">Road map</a></li>
+            <li><a href="index.html#download">Download</a></li>
+            <li><a href="index.html#airDrop">AirDrop</a></li>
+          </ul>
+          <div class="footer__desc wow fadeInUp">LocalCoin is THE true democracy decentralized ecosystem where everyone is free to do what they want and to say what they want. LocalCoin is not a company or a person, it is a community of people who shares the same libertarian values and freedoms.</div>
+        </div>
+      </footer>
+      <div class="popup-modal">
+        <div class="p-modal">
+          <div class="p-modal__close"></div>
+          <div class="p-modal__form">
+            <form class="ps-form" action="#">
+              <div class="ps-form__input ps-form__input_num input-coin">
+                <input type="number" name="btc" value="0" min="0">
+                <div class="ps-form__select select select-coin">
+                  <input class="select_hidden" type="hidden" value="1">
+                  <div class="select_in select-coin_in"><span class="select_title select-coin_title">BTC</span><i class="select_arrow select-coin_arrow"></i></div>
+                  <ul class="select_list select-coin_list">
+                    <li class="is-active" data-value="1">BTC</li>
+                    <li data-value="2">CTC</li>
+                    <li data-value="3">DTC</li>
+                    <li data-value="4">ETC</li>
+                    <li data-value="5">FTC</li>
+                    <li data-value="6">GTC</li>
+                  </ul>
+                </div>
+                <div class="prev-coin"></div>
+                <div class="next-coin"></div>
+              </div>
+              <div class="ps-form__arr"></div>
+              <div class="ps-form__input ps-form__input_num input-llc">
+                <input type="number" name="llc" value="0" min="0">
+              </div>
+              <div class="ps-form__input ps-form__input_text">
+                <input type="text" placeholder="Your LocalCoin Account name"><span class="ps-form__reg">Not a member yet? <a href="https://wallet.localcoin.is/">Register now</a></span>
+              </div>
+              <input class="ps-form__btn btn-yellow" type="submit" value="Buy LLC coin">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- jQuery-->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <!-- Plugins-->
+    <script src="js/wow.min.js"></script>
+    <!-- JavaScripts-->
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
