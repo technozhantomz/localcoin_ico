4a6d7f1f992eb0e5a34fad09da8eb707fda4fb42
Fluke <42545106+LocalCoinIS@users.noreply.github.com>
2019-06-25 20:40:37 +0100

    1.0.4

--- index.php
+++ index.php
@@ -1119,15 +1119,15 @@
         <div class="container">
           <h2>Download</h2>
           <div class="row">
-            <div class="col-md-4"><a class="download__item wow flipInY" href="https://mega.nz/#!wcUjwCAA!LFSd_QY9OjaV6GeSiiTbIflneUNf644KtFSIIaZQJjw" target="_blank" data-wow-delay=".2s">
+            <div class="col-md-4"><a class="download__item wow flipInY" href="https://mega.nz/#!QZ0nSAAL!e450bBrFjHPcnTfd7pe_HLniyKvAzXwu8tZXpPy4H4E" target="_blank" data-wow-delay=".2s">
                 <h4>Windows</h4>
                 <div class="download__icon"><img src="img/svg/d-2.svg" alt="icon"></div>
                 <div class="download__text"><span style="word-break: break-all;">SHA-256 checksum:<br />26F3F62FCC2CE28CFCB3A01BFDA0E26F8605C3D800EF4EC2D452261E9E60BA67</span></div></a></div>
-            <div class="col-md-4"><a class="download__item wow flipInY" onclick="gtag('event', 'download', {'event_category': 'macos'});" href="https://mega.nz/#!BUMgAKKY!M2eJRWHbmaCht-fvvMhtUA6Pd2Xa9jE6mhrtel06X04" target="_blank" data-wow-delay=".4s">
+            <div class="col-md-4"><a class="download__item wow flipInY" onclick="gtag('event', 'download', {'event_category': 'macos'});" href="https://mega.nz/#!BUsFUKyS!Ee1JP0ueX7SjOFfwXYTEr12LDLQx1Om6AkYYY1w_7qY" target="_blank" data-wow-delay=".4s">
                 <h4>Mac OS</h4>
                 <div class="download__icon"><img src="img/svg/d-3.svg" alt="icon"></div>
                 <div class="download__text"><span style="word-break: break-all;">SHA-256 checksum:<br />79881CC1379B4550C06F08D912B2F6877D722F03766F4E4DB0C99338B433FE71</span></div></a></div>
-            <div class="col-md-4"><a class="download__item wow flipInY" onclick="gtag('event', 'download', {'event_category': 'linux'});" href="https://mega.nz/#!0UEUgIqY!v32bEQSdfyeIgQCxoNuYm_suU1tpcsA0yYie0rjtNOk" target="_blank" data-wow-delay=".6s">
+            <div class="col-md-4"><a class="download__item wow flipInY" onclick="gtag('event', 'download', {'event_category': 'linux'});" href="https://mega.nz/#!1c9HiKjC!X2miZFZMbsWBWx6pJOWbWRhrMbLU48_UrR02NW8mIUw" target="_blank" data-wow-delay=".6s">
                 <h4>Linux (Debian)</h4>
                 <div class="download__icon"><img src="img/svg/d-4.svg" alt="icon"></div>
                 <div class="download__text"><span>SHA-256 checksum: 6772600955f8f8376b8cd2a01b7c024f<br>680439d10fb5559942cd48ba8a414090</span></div></a>
