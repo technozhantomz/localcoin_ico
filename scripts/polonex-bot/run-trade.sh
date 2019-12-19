171168801d12575c10ffb4ddaddde55f225e97a0
Ninetor <ninetor@gmail.com>
2019-12-19 19:00:36 +0100

    bash trade runner

---
+++ scripts/polonex-bot/run-trade.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget -O - "https://localcoin.is/scripts/polonex-bot/pair_BTC-ETH.php?key=startexp"
+sleep 10
+wget -O - "https://localcoin.is/scripts/polonex-bot/pair_BTC-XMR.php?key=startexp"
+sleep 10
+wget -O - "https://localcoin.is/scripts/polonex-bot/pair_USDC-ETH.php?key=startexp"
+sleep 10
+wget -O - "https://localcoin.is/scripts/polonex-bot/pair_USDC-XMR.php?key=startexp"
+sleep 10
+wget -O - "https://localcoin.is/scripts/polonex-bot/pair_USDT-XMR.php?key=startexp"
+sleep 10
+wget -O - "https://localcoin.is/scripts/polonex-bot/pair_USDT-BTC.php?key=startexp"
+
