8c6684a8c67e47abdf46e8f746f19c05fbfad95b
Nineseven <nine@nineseven.ru>
2019-12-20 19:04:58 +0300

    Pump to all pairs

--- scripts/polonex-bot/run-pump.sh
+++ scripts/polonex-bot/run-pump.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 sleep $[ ( $RANDOM % 119 )  + 1 ]s
-wget -O - "https://localcoin.is/scripts/polonex-bot/pump_BTC-XMR.php?key=startexp"
+wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=BTC&pair2=ETH"
+wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=BTC&pair2=XMR"
+wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=USDC&pair2=ETH"
+wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=USDC&pair2=XMR"
+wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=USDC&pair2=BTC"
+wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=USDT&pair2=BTC"
+wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=USDT&pair2=ETH"
+wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=USDT&pair2=XMR"
