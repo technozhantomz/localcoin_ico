8178fd2dcf9e24768dfbbee0b7dac990cc6b9e6a
Ninetor <ninetor@gmail.com>
2019-12-23 15:07:41 +0100

    Pumper timer change

--- scripts/polonex-bot/run-pump.sh
+++ scripts/polonex-bot/run-pump.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sleep $[ ( $RANDOM % 119 )  + 1 ]s
+sleep $[ ( $RANDOM % 899 )  + 1 ]s
 wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=BTC&pair2=ETH"
-sleep $[ ( $RANDOM % 119 )  + 1 ]s
-wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=BTC&pair2=XMR"
