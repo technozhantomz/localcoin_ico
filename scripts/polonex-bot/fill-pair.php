4633da18d9614cff4d998b58651e0e41379b4db8
Nineseven <nine@nineseven.ru>
2019-12-24 14:41:35 +0300

    USDT pumper

--- scripts/polonex-bot/fill-pair.php
+++ scripts/polonex-bot/fill-pair.php
@@ -5,14 +5,14 @@ if ($_GET['key'] != 'startexp') die('invalid key');
 $fillpair = new Bot($_GET['pair1'], $_GET['pair2'],'30');
 $fillpair->run();
 
-//?key=startexp&pair1=BTC&pair2=ETH
-//?key=startexp&pair1=BTC&pair2=XMR
+//?key=startexp&pair1=BTC&pair2=ETH +
+//?key=startexp&pair1=BTC&pair2=XMR +
 
 //?key=startexp&pair1=USDC&pair2=ETH
 //?key=startexp&pair1=USDC&pair2=XMR
-//?key=startexp&pair1=USDC&pair2=BTC
+//?key=startexp&pair1=USDC&pair2=BTC +
 
-//?key=startexp&pair1=USDT&pair2=BTC
+//?key=startexp&pair1=USDT&pair2=BTC +
 //?key=startexp&pair1=USDT&pair2=ETH
 //?key=startexp&pair1=USDT&pair2=XMR
 
---
+++ scripts/polonex-bot/run-pump-USDT-XMR.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sleep $[ ( $RANDOM % 899 )  + 1 ]s
+wget -O - "https://localcoin.is/scripts/polonex-bot/pump-pair.php?key=startexp&pair1=USDT&pair2=ETH"
